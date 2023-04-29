40c6cd00f78e4099c69442cd10e98af11db7b778
josh-quatela <57924778+josh-quatela@users.noreply.github.com>
2023-04-29 16:07:44 -0400

    josh-patch-1 added logging in/logging out logic
    josh-patch-1 added logging in/logging out logic

--- app.py
+++ app.py
@@ -4,15 +4,23 @@ app = Flask(__name__)
 
 # Sample user data
 users = [
-    {'username': 'user1', 'password': 'pass1'},
-    {'username': 'user2', 'password': 'pass2'},
-    {'username': 'user3', 'password': 'pass3'}
+    {'username': 'user1', 'email': 'email1', 'password': 'pass1'},
+    {'username': 'user2', 'email': 'email2', 'password': 'pass2'},
+    {'username': 'user3', 'email': 'email3', 'password': 'pass3'}
 ]
 
 
 @app.route('/')
 def index():
-    return render_template('/Frontend/home.html')
+    return render_template('home.html')
+
+
+@app.route('/login_home', methods=['GET', 'POST'])
+def login_home():
+    if request.method == 'POST':
+        return redirect(url_for("login"))
+    else:
+        return render_template('login_home.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -23,22 +31,32 @@ def login():
         password = request.form['pass_input']
         email = request.form['email_input']
 
+        print("USER: ", username)
+        print("PASS: ", password)
+        print("EMAIL: ", email)
+
         # Check if the user is in the user list
         for user in users:
-            if user['username'] == username and user['password'] == password:
+            if user['username'] == username and user['password'] == password and user['email'] == email:
                 # User is authenticated, redirect to the home page
                 return redirect(url_for('home'))
 
         # User is not authenticated, show error message
         error = 'Invalid credentials. Please try again.'
-        return render_template('login.html', error=error)
+        return render_template('login_home.html', error=error)
 
-    return render_template('login.html')
+    return render_template('home_logged_in.html')
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    print("GOT HERE")
+    return render_template('home.html')
 
 
 @app.route('/home')
 def home():
-    return render_template('/Frontend/home.html')
+    return render_template('home_logged_in.html')
 
 
 if __name__ == '__main__':
